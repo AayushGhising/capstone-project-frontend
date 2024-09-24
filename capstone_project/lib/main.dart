f58e973487dff413b30d43fc886ace3bb6d734b7
Aayush Ghising <aayushghising@gmail.com>
2024-09-24 13:35:01 +0545

    changed help and faqs

--- capstone_project/lib/main.dart
+++ capstone_project/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:capstone_project/loading_screen.dart';
+import 'package:capstone_project/help_and_faqs.dart';
 
 void main() {
   runApp(const MaterialApp(
     home: Scaffold(
+      // body: LoadingScreen(),
       body: LoadingScreen(),
     ),
   ));
